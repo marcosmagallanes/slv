57f7cec9168277b7652af2d16e6d3181cf1a9ed0
marcosmagallanes <marcos.m.magallanes@gmail.com>
2024-03-27 15:23:42 -0600

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+const restaurantCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  restaurants: restaurantCollection,
+};
